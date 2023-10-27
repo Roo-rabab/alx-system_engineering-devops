10c354a0db6202d75ef80e4797b4fe5c77eaa6e9
Roo-rabab <rababmchich312@gmail.com>
2023-10-27 13:51:31 -0700

    wtf

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Define a package resource to install Flask using pip3
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
