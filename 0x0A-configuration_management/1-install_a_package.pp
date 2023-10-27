46baf3c85f3d577186f498d457bbf7b8629c5464
Roo-rabab <rababmchich312@gmail.com>
2023-10-27 12:39:02 -0700

    GaBi

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Using Puppet, install flask from pip3.
+# Requirements:
+#  Install flask
+#  Version must be 2.1.0
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
